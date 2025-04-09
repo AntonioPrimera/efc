f4155862f975d3bc8f99a41914ad706092a1237e
Antonio Primera <antonio@cus.ro>
2025-04-09 14:52:31 +0300

    Added InvoiceType::NotaDeCreditare

--- src/Enums/InvoiceType.php
+++ src/Enums/InvoiceType.php
@@ -4,6 +4,7 @@ namespace AntonioPrimera\Efc\Enums;
 enum InvoiceType: int
 {
     case Factura = 380;
+    case NotaDeCreditare = 381;
     case FacturaCorectata = 384;
     case AutoFactura = 389;
     case FacturaInformare = 751;
